448b1543e2c5fe2f9db1d34e2e53f56e4ad22d56
Tai_Hello R <88864110+vantai2311@users.noreply.github.com>
2021-09-24 12:57:14 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,17 @@
 
 ## 
 
-makeCacheMatrix <- function(x = matrix()) { ## define the argument with default mode of "matrix"
-    inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
-    set <- function(y) {                    ## define the set function to assign new 
-        x <<- y                             ## value of matrix in parent environment
-        inv <<- NULL                        ## if there is a new matrix, reset inv to NULL
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL                             
+    set <- function(y) {                    
+        x <<- y                             
+        inv <<- NULL                       
     }
-    get <- function() x                     ## define the get fucntion - returns value of the matrix argument
+    get <- function() x                    
 
-    setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
-    getinverse <- function() inv                     ## gets the value of inv where called
-    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
-                                                                                  ## to the functions with the $ operator
+    setinverse <- function(inverse) inv <<- inverse  
+    getinverse <- function() inv                    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)                                                                                  
 }
 
 
